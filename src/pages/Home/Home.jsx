72dbcf1dfdce8fa92a1d397e7aec1cc2b41f49de
tabib-e-alahi <etabib.alahi@gmail.com>
2023-10-06 20:15:07 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+// import PropTypes from 'prop-types';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-3xl text-center">This is Home</h2>
+        </div>
+    );
+};
+
+Home.propTypes = {
+    
+};
+
+export default Home;
---
+++ src/pages/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-center">This is nav</h2>
+        </div>
+    );
+};
+
+export default Navbar;
